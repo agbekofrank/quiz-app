eae608c96152529dadf89cedba4d62fb1a21c2ed
phranc <frank.agbeko@amalitech.org>
2021-04-15 15:18:05 +0000

    initial Commit

---
+++ js/mathematics.js
@@ -0,0 +1,27 @@
+
+fetch(
+    'https://opentdb.com/api.php?amount=5&category=19&type=multiple'
+).then(res => {
+    // console.log(res);
+    return res.json();
+
+}).then(loadedQuestions => {
+    // console.log(loadedQuestions.results);
+    questions = loadedQuestions.results.map((loadedQuestion) => {
+        const formattedQuestion = {
+            question: loadedQuestion.question,
+        }
+        const answerChoices = [...loadedQuestion.incorrect_answers];
+        formattedQuestion.answer = Math.floor(Math.random() * 4) + 1;
+        answerChoices.splice(formattedQuestion.answer - 1, 0, loadedQuestion.correct_answer);
+        answerChoices.forEach((choice, index) => {
+            formattedQuestion["choice" + (index + 1)] = choice;
+        })
+        // console.log(answerChoices);
+        return formattedQuestion;
+    });
+    startGame();
+}).catch(err => {
+    console.error(err);
+
+})
